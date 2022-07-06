cf266c3e0625643f2340e64e9208b02af25aa4fa
vadzim-z <vadzim.zylevich@maersk.com>
2022-07-06 11:17:04 +0200

    Refactoring

--- Program.cs
+++ Program.cs
@@ -3,12 +3,11 @@ using NBomber.Configuration;
 using NBomber.CSharp;
 using NBomber.Plugins.Http.CSharp;
 
-var transportOrderNumber = "AUTOFAnPbfk5lq";
 var step = Step.Create("Fetch_transportOrderError_records", 
     clientFactory: HttpClientFactory.Create(),
     execute: async context =>
     {
-        var json = await LoadJsonAsync("TransportOrderErrors.json");
+        var json = await LoadJsonAsync();
         var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
         var request = Http.CreateRequest("POST", "https://localhost:7169/graphql")
             .WithHeader("Content-Type", "application/json")
@@ -17,58 +16,22 @@ var step = Step.Create("Fetch_transportOrderError_records",
         return await Http.Send(request, context);
     });
 
-var restApiStep = Step.Create("Fetch_transportOrder_by_Id-RestApi", 
-    clientFactory: HttpClientFactory.Create(),
-    execute: async context =>
-    {
-        var url = $"http://localhost:5000/api/transport-orders/{transportOrderNumber}/history";
-        var request = Http.CreateRequest("GET", url)
-            .WithHeader("Service-Key", "asdfasdflkjasdf")
-            ;
-
-        return await Http.Send(request, context);
-    });
-var graphQLStep = Step.Create("Fetch_transportOrder_by_Id-GraphQL", 
-    clientFactory: HttpClientFactory.Create(),
-    execute: async context =>
-    {
-        var json = await LoadJsonAsync("TransportOrderHistory.json");
-        var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
-        var request = Http.CreateRequest("POST", "http://localhost:5000/graphql")
-            .WithHeader("Content-Type", "application/json")
-            .WithBody(httpContent);
-
-        return await Http.Send(request, context);
-    });
-
-var restApiScenario = ScenarioBuilder
-    .CreateScenario("TransportOrder RestApi", restApiStep)
-    .WithWarmUpDuration(TimeSpan.FromSeconds(5))
-    .WithLoadSimulations(
-        Simulation.InjectPerSec(rate: 100, during: TimeSpan.FromSeconds(30)));
-
-var graphQLScenario = ScenarioBuilder
-    .CreateScenario("TransportOrder GraphQL", graphQLStep)
-    .WithWarmUpDuration(TimeSpan.FromSeconds(5))
-    .WithLoadSimulations(
-        Simulation.InjectPerSec(rate: 100, during: TimeSpan.FromSeconds(30)));
-
-var errorHandlingScenario = ScenarioBuilder
-    .CreateScenario("TransportOrderError GraphQL", graphQLStep)
+var scenario = ScenarioBuilder
+    .CreateScenario("Simple_GraphQL", step)
     .WithWarmUpDuration(TimeSpan.FromSeconds(5))
     .WithLoadSimulations(
         Simulation.InjectPerSec(rate: 100, during: TimeSpan.FromSeconds(30)));
 
 NBomberRunner
-    .RegisterScenarios(errorHandlingScenario/*, restApiScenario, graphQLScenario*/)
+    .RegisterScenarios(scenario)
     .WithReportFormats(ReportFormat.Html, ReportFormat.Md)
     .Run();
 
-async Task<string> LoadJsonAsync(string path)
+async Task<string> LoadJsonAsync()
 {
     var json = String.Empty;
 
-    using var r = new StreamReader(path);
+    using var r = new StreamReader("TransportOrderErrors.json");
     {
         json = await r.ReadToEndAsync();
     }
